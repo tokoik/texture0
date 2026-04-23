83a992f7fc9e9b902a650f6dc40eecb404714159
Kohe Tokoi <tokoi@wakayama-u.ac.jp>
2026-04-23 11:17:41 +0900

    docs: update README and adjust main application entry point configuration

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #else
 #  if defined(_WIN32)
 #    define _CRT_SECURE_NO_WARNINGS
+#    define GL_CLAMP_TO_EDGE 0x812F
 //#    pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 #  endif
 #  include <GL/glut.h>
